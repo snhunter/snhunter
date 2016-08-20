177366b398ee6d6bb44f8e963f9ca33efcc6558b
Tobias Kohler <i-lispeacs@gmx.net>
2016-08-20 18:49:40 +0200

    second commit

---
+++ snh.py
@@ -0,0 +1,31 @@
+import sys
+import scipy.misc
+from matplotlib import cm
+from PIL import Image
+
+
+#Parse command line arguments
+filename = sys.argv[1]
+basename = filename.split('.')
+
+
+#Read input image
+im = scipy.misc.imread(filename, flatten=True)
+
+#Read crosshair overlay
+overlay = Image.open("data/snhunter/crosshair.png")
+
+
+#Apply gamma correction to linear image data from PANSTARRS
+im = (im/255.)**(2.2)
+
+#Apply perceptually uniform (CAM02UCS color space) and colorblindness-friendly colormap
+#See https://bids.github.io/colormap/images/screenshots/option_a.png
+#Change magma to gray for grayscale colormap
+im = Image.fromarray(cm.magma(im, bytes=True))
+
+#Merge background with overlay
+im = Image.alpha_composite(im, overlay)
+
+#Save final result
+im.save(basename[0] + "_gamma.jpg", quality=93, optimize=True, progressive=True)
